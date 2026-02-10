dd387b0b0d9fa0294eb633e6adc36c8c1a6315e2
Anton Datsuk <anton.datsuk@gmail.com>
2026-02-10 14:55:02 +0300

    fix relative path issue

--- tools/get_code_coverage.sh
+++ tools/get_code_coverage.sh
@@ -2,11 +2,10 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-BUILD_DIR="${ROOT_DIR}/tests/build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug"
 
-# If you run coverage from another build dir, allow override:
-# BUILD_DIR=/path/to/build ./tools/cov.sh
-BUILD_DIR="${BUILD_DIR_OVERRIDE:-$BUILD_DIR}"
+BUILD_DIR="${BUILD_DIR_OVERRIDE:-${1:-.}}"
+
+BUILD_DIR="$(cd "$BUILD_DIR" && pwd)"
 
 cd "$BUILD_DIR"
 
@@ -15,4 +14,3 @@ gcovr -r "$ROOT_DIR" \
   --exclude ".*[\\/](moc_|ui_|qrc_).*" \
   --exclude ".*[\\/]tests[\\/].*" \
 | awk 'NF>=4{print $1,$2,$3,$4}' | column -t
-
